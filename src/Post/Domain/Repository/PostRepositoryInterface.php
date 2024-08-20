28f012423ad767fd13f2717190f42f1fd74ea161
xamayca <xamalekoum@gmail.com>
2024-08-21 01:28:04 +0200

    add comments to all functions

--- src/Post/Domain/Repository/PostRepositoryInterface.php
+++ src/Post/Domain/Repository/PostRepositoryInterface.php
@@ -15,19 +15,44 @@ use App\Post\Domain\ValueObject\PostId;
  * * - remove(PostId $id) : supprime un post
  * * - get(PostId $id) : récupère un post par son identifiant
  * * - all() : récupère tous les posts
- * * - generateId() : génère un identifiant de post (en général, un UUID)
+ * * - generateId() : génère un identifiant de post (en général, un UUID ou un entier auto-incrémenté)
  */
 interface PostRepositoryInterface
 {
+    /**
+     * Ajoute un post
+     * @param Post $post
+     */
     public function add(Post $post): void;
 
+    /**
+     * Met à jour un post
+     * @param Post $post
+     */
     public function update(Post $post): void;
 
+    /**
+     * Supprime un post
+     * @param PostId $id
+     */
     public function remove(PostId $id): void;
 
+    /**
+     * Récupère un post par son identifiant
+     * @param PostId $id
+     * @return Post|null
+     */
     public function get(PostId $id): ?Post;
 
+    /**
+     * Récupère tous les posts
+     * @return array
+     */
     public function all(): array;
 
+    /**
+     * Génère un identifiant de post (en général, un UUID ou un entier auto-incrémenté)
+     * @return PostId
+     */
     public function generateId(): PostId;
 }
