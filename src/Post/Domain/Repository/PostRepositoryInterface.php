de28892d5a91094fc33653b93b90a0a1c3068c1f
xamayca <xamalekoum@gmail.com>
2024-09-14 10:53:58 +0200

    fix comments

--- src/Post/Domain/Repository/PostRepositoryInterface.php
+++ src/Post/Domain/Repository/PostRepositoryInterface.php
@@ -10,10 +10,10 @@ use App\Post\Domain\ValueObject\PostId;
  * @package App\Post\Domain\Repository
  * Cette interface représente le contrat que doit respecter un repository de post.
  * * Elle contient les méthodes suivantes :
- * * - add(Post $post) : ajoute un post
- * * - update(Post $post) : met à jour un post
- * * - remove(PostId $id) : supprime un post
- * * - get(PostId $id) : récupère un post par son identifiant
+ * * - add (Post $post) : ajoute un post
+ * * - update (Post $post) : met à jour un post
+ * * - remove (PostId $id) : supprime un post
+ * * - get (PostId $id) : récupère un post par son identifiant
  * * - all() : récupère tous les posts
  * * - generateId() : génère un identifiant de post (en général, un UUID ou un entier auto-incrémenté)
  */
